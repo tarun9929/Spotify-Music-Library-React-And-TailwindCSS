f5aded7f1e0231fc200dc70845dd1b54182ef30b
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2025-12-07 08:46:16 +0000

    feat: initialize React project with Vite and TailwindCSS
    - Added package.json with dependencies for React, TailwindCSS, and Vite.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.jsx) with a title.
    - Added React logo SVG in assets directory.
    - Set up global CSS file to import TailwindCSS styles.
    - Configured main entry point (main.jsx) for React application.
    - Created Vite configuration file (vite.config.js) with React and TailwindCSS plugins.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-xl text-white text-center mt-6'>Spotify Music Library React and TailwindCSS</h1>
+    </>
+  )
+}
+
+export default App
